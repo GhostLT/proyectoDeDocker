16738d3042adc18f82dac2946ea450c670b3ee97
GhostLT <viverosjavier86@gmail.com>
2024-07-19 18:13:28 -0500

    Esta es la estructura base de un proyecto API REST y esta como base Docker PHP 8, MariaDB, PHPMYAdmin, Nginx

--- app/index.php
+++ app/index.php
@@ -4,7 +4,8 @@
 
 $json = array(
     "status" => 200,
-    "result" => "success"
+    "result" => "success",
+    "result" => "Este proyecto es una estructura base MVC para un API REST, esta hecho con la configuracion de Docker con las tecnologias Nginx,MYSQL,PHPMyAdmin,PHP 8"
 );
 
 echo json_encode($json);
